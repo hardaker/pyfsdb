5b20f89492bf3c1462d5bc520fb086ea68fee7cc
Wes Hardaker <opensource@hardakers.net>
2020-04-01 15:20:07 -0700

    test fsdb_to_json

--- pyfsdb/tests/test_json.py
+++ pyfsdb/tests/test_json.py
@@ -14,3 +14,16 @@ class test_json_functions(unittest.TestCase):
                          "#fsdb -F t a d\nc\tf\nb\te\n",
                          "output of json_to_fsdb is correct")
         
+    def test_fsdb_to_json(self):
+        from pyfsdb.fsdb2json import fsdb_to_json
+        self.assertTrue(fsdb_to_json, "loaded")
+
+        inp = StringIO("#fsdb -F t a d\nc\tf\nb\te\n")
+        output = StringIO()
+
+        fsdb_to_json(inp, output)
+
+        self.assertEqual(output.getvalue(),
+                         '{"a": "c", "d": "f"}' + "\n" + \
+                         '{"a": "b", "d": "e"}' + "\n",
+                         "output of fsdb_to_json is correct")
