a824cc2d40e1c567e6d6483e4287679d7a3a098b
Wes Hardaker <opensource@hardakers.net>
2020-12-15 14:29:19 -0800

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyfsdb",
-    version="1.1.7",
+    version="1.1.8",
     author="Wes Hardaker",
     author_email="opensource@hardakers.net",
     description="A python implementation of the flat-file streaming database",
