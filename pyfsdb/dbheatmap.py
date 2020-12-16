53f2bd658dbf307fba376b38bd2aa61e567fc62d
Wes Hardaker <opensource@hardakers.net>
2020-12-16 11:11:19 -0800

    use agg to avoid display requirement

--- pyfsdb/dbheatmap.py
+++ pyfsdb/dbheatmap.py
@@ -4,9 +4,9 @@ import sys
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, FileType
 
 import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
-
 import pyfsdb
 
 
