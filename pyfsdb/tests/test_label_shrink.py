82183716d820276a77c70a510cba0f8495e5cb5d
Wes Hardaker <opensource@hardakers.net>
2021-01-15 14:47:31 -0800

    fix label shrink test

--- pyfsdb/tests/test_label_shrink.py
+++ pyfsdb/tests/test_label_shrink.py
@@ -5,9 +5,9 @@ def test_label_shrink():
     assert maybe_shrink_label("foo") == "foo"
     assert maybe_shrink_label("o" * 20) == "o" * 20
     assert maybe_shrink_label("o" * 10 + "p" * 10) == "o" * 10 + "p" * 10
-    assert maybe_shrink_label("o" * 11 + "p" * 11) == \
+    assert maybe_shrink_label("o" * 11 + "p" * 11, 20) == \
         "o" * 9 + "..." + "p" * 8
-    assert maybe_shrink_label("o" * 10 + "p" * 11) == \
+    assert maybe_shrink_label("o" * 10 + "p" * 11, 20) == \
         "o" * 9 + "..." + "p" * 8
-    assert maybe_shrink_label("o" * 100 + "p" * 11000) == \
+    assert maybe_shrink_label("o" * 100 + "p" * 11000, 20) == \
         "o" * 9 + "..." + "p" * 8
