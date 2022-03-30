f4ed83ab495a48632532f5f320d71f37ea384d45
Wes Hardaker <opensource@hardakers.net>
2022-03-30 06:26:38 -0700

    tests for adding types

---
+++ pyfsdb/tests/test_add_types.py
@@ -0,0 +1,27 @@
+import unittest
+from io import StringIO
+from pyfsdb.tools.pdbaddtypes import add_types
+from unittest.mock import Mock
+from test_fsdb_class import truncate_comments
+
+class test_add_types(unittest.TestCase):
+    def test_add_single_type(self):
+        indata = StringIO("#fsdb -F s a b c\na 1 2.3")
+        outdata = StringIO()
+        outdata.close = Mock()
+        add_types(indata, outdata, ['b=l'])
+        self.assertEqual(truncate_comments(outdata.getvalue()),
+                         "#fsdb -F s a b:l c\na 1 2.3")
+
+    def test_add_multiple_types(self):
+        indata = StringIO("#fsdb -F s a b c\na 1 2.3")
+        outdata = StringIO()
+        outdata.close = Mock()
+        add_types(indata, outdata, ['b=l', 'c=d'])
+        self.assertEqual(truncate_comments(outdata.getvalue()),
+                         "#fsdb -F s a b:l c:d\na 1 2.3")
+
+
+if __name__ == "__main__":
+    import unittest
+    unittest.main()
