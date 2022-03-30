cee0d4fb447fa3b69f5c594738c4064a83559c7f
Wes Hardaker <opensource@hardakers.net>
2022-03-30 06:30:07 -0700

    more functionality tests

--- pyfsdb/tests/test_add_types.py
+++ pyfsdb/tests/test_add_types.py
@@ -21,6 +21,22 @@ class test_add_types(unittest.TestCase):
         self.assertEqual(truncate_comments(outdata.getvalue()),
                          "#fsdb -F s a b:l c:d\na 1 2.3")
 
+    def test_merge_types(self):
+        indata = StringIO("#fsdb -F s a b:l c\na 1 2.3")
+        outdata = StringIO()
+        outdata.close = Mock()
+        add_types(indata, outdata, ['c=d'])
+        self.assertEqual(truncate_comments(outdata.getvalue()),
+                         "#fsdb -F s a b:l c:d\na 1 2.3")
+
+    def test_override_types(self):
+        indata = StringIO("#fsdb -F s a b:l c:d\na 1 2.3")
+        outdata = StringIO()
+        outdata.close = Mock()
+        add_types(indata, outdata, ['b=d'])
+        self.assertEqual(truncate_comments(outdata.getvalue()),
+                         "#fsdb -F s a b:d c:d\na 1 2.3")
+
 
 if __name__ == "__main__":
     import unittest
