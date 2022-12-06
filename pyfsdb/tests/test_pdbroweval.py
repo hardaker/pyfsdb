34dc264d3f13da66a067fa293367ae35549caba7
Wes Hardaker <opensource@hardakers.net>
2022-12-06 11:48:03 -0800

    start pdfroweval

---
+++ pyfsdb/tests/test_pdbroweval.py
@@ -0,0 +1,51 @@
+from io import StringIO
+import unittest
+import pyfsdb
+from logging import error
+
+def noop():
+    pass
+
+class test_pdbroweval(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(test_pdbroweval, self).__init__(*args, **kwargs)
+        self.input_data = [
+            {'a': 5, 'b': 10, 'c': 15},
+            {'a': 2, 'b': 4, 'c': 8},
+            {'a': 3, 'b': 6, 'c': 9},            
+        ]
+
+    def test_load(self):
+        from pyfsdb.tools.pdbroweval import process_pdbroweval
+        self.assertTrue(True, "could load")
+
+    def convert_to_stringio(self, data):
+        outh = StringIO()
+        outh.close = noop
+        oh = pyfsdb.Fsdb(out_file_handle=outh)
+        oh.out_column_names = list(data[0].keys())
+        for row in data:
+            oh.append(list(row.values()))
+        oh.close()
+        return StringIO(outh.getvalue())
+
+    def get_standard_input(self):
+        return self.convert_to_stringio(self.input_data)
+
+    def base_test_and_eval(self, expression, expected_result, init_code=None):
+        from pyfsdb.tools.pdbroweval import process_pdbroweval
+
+        input_data_fsdb = self.get_standard_input()
+        output_data = StringIO()
+        output_data.close = noop
+
+        process_pdbroweval(input_data_fsdb, output_data, expression, init_code=init_code)
+        
+        data = pyfsdb.Fsdb(file_handle=StringIO(output_data.getvalue()),
+                           return_type=pyfsdb.RETURN_AS_DICTIONARY).get_all()
+
+        self.assertEqual(data, expected_result)
+
+    def test_true_no_changes(self):
+        self.base_test_and_eval("pass", self.input_data)
+
---
+++ pyfsdb/tools/pdbroweval.py
@@ -0,0 +1,94 @@
+"""Selects rows based on python boolean expressions"""
+
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, FileType
+from logging import debug, info, warning, error, critical
+import logging
+import sys
+import pyfsdb
+
+
+def parse_args():
+    parser = ArgumentParser(
+        formatter_class=ArgumentDefaultsHelpFormatter,
+        description=__doc__,
+        epilog="Exmaple Usage: pdbrow 'column_a == 5'",
+    )
+
+    parser.add_argument("-i", "--init-code",
+                        help="Initialization code to execute first (eg, imports)")
+
+    parser.add_argument(
+        "--log-level", default="info", help="Define the logging verbosity level."
+    )
+
+    parser.add_argument(
+        "expression",
+        help="The boolean expression to evaluate",
+    )
+
+    parser.add_argument(
+        "input_file",
+        type=FileType("r"),
+        nargs="?",
+        default=sys.stdin,
+        help="The input FSDB file",
+    )
+
+    parser.add_argument(
+        "output_file",
+        type=FileType("w"),
+        nargs="?",
+        default=sys.stdout,
+        help="Where to write the output FSDB to",
+    )
+
+    args = parser.parse_args()
+    log_level = args.log_level.upper()
+    logging.basicConfig(level=log_level, format="%(levelname)-10s:\t%(message)s")
+    return args
+
+
+def process_pdbroweval(
+        input_file,
+        output_file,
+        expression,
+        init_code = None,
+):
+
+    # open input and output fsdb handles
+    fh = pyfsdb.Fsdb(file_handle=input_file, return_type=pyfsdb.RETURN_AS_DICTIONARY)
+    oh = pyfsdb.Fsdb(out_file_handle=output_file)
+
+    # crate output columns
+    oh.out_column_names = fh.column_names
+
+    globals = {}
+
+    if init_code:
+        exec(compile(init_code, '<string>', 'exec'), globals)
+
+    compiled_expression = compile(f"{expression}", '<string>', 'exec')
+
+    # process the rows
+    for row in fh:
+
+        # execute the expression and check its result
+        exec(compiled_expression, globals, row)
+        oh.append(row)
+
+    oh.close()
+
+def main():
+    args = parse_args()
+
+    process_pdbrow(
+        args.input_file,
+        args.output_file,
+        args.expression,
+        args.init_code,
+    )
+
+
+if __name__ == "__main__":
+    main()
+
