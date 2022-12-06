9c97539b39592563f34c72d8e41a669aaaea1ea9
Wes Hardaker <opensource@hardakers.net>
2022-12-06 08:53:29 -0800

    make pdbrow actually work

--- pyfsdb/tools/pdbrow.py
+++ pyfsdb/tools/pdbrow.py
@@ -11,51 +11,16 @@ def parse_args():
     parser = ArgumentParser(
         formatter_class=ArgumentDefaultsHelpFormatter,
         description=__doc__,
-        epilog="Exmaple Usage: ",
-    )
-
-    parser.add_argument(
-        "-c",
-        "--data-column",
-        default="data",
-        type=str,
-        help="The input column name to create a CDF from",
-    )
-
-    parser.add_argument(
-        "-C",
-        "--cdf-column",
-        default=None,
-        type=str,
-        help="The output CDF column name -- if none, '_cdf' will be appended to --column",
-    )
-
-    parser.add_argument(
-        "-R",
-        "--raw-column",
-        default=None,
-        type=str,
-        help="Output raw accumulating count to this column",
-    )
-
-    parser.add_argument(
-        "-P",
-        "--percent-column",
-        default=None,
-        type=str,
-        help="Output a percentage column, in addition to the CDF column",
-    )
-
-    parser.add_argument(
-        "-F",
-        "--fraction-column",
-        default=None,
-        type=str,
-        help="Output a percentage column, in addition to the CDF column",
+        epilog="Exmaple Usage: pdbrow 'column_a == 5'",
     )
 
     parser.add_argument(
         "--log-level", default="info", help="Define the logging verbosity level."
+    )
+
+    parser.add_argument(
+        "expression",
+        help="The boolean expression to evaluate",
     )
 
     parser.add_argument(
