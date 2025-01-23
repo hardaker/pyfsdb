20148cf4252eea9c755fc6eb556ee49bf3436aba
Wes Hardaker <opensource@hardakers.net>
2025-01-23 12:46:28 -0800

    failing utf-8 file test

--- pyfsdb/tests/test_utf8.py
+++ pyfsdb/tests/test_utf8.py
@@ -1,5 +1,18 @@
+import pytest
 import pyfsdb
 from io import StringIO
+
+
+DATA = "#fsdb -F t test:i copy©:i foo:a\n1\t2\t3\n4\t5\t©\n"
+
+
+@pytest.fixture
+def create_file(tmp_path):
+    tmp_file = tmp_path / "test.fsdb"
+    fh = open(tmp_file, "wb")
+    fh.write(bytes(DATA, "utf-8"))
+    fh.close()
+    yield tmp_file
 
 
 def do_test_utf8_file_handle(fh):
@@ -11,9 +24,12 @@ def do_test_utf8_file_handle(fh):
     assert row == [4, 5, "©"]
 
 
-def test_utf8_support():
-    DATA = "#fsdb -F t test:i copy©:i foo:a\n1\t2\t3\n4\t5\t©\n"
-
+def test_utf8_support_stringio():
     DATA_stream = StringIO(DATA)
     fh = pyfsdb.Fsdb(file_handle=DATA_stream)
+    do_test_utf8_file_handle(fh)
+
+
+def test_utf8_support_file(create_file):
+    fh = pyfsdb.Fsdb(create_file)
     do_test_utf8_file_handle(fh)
