2a37fa106075d09f4f86daab0bdc5cd73df7e03b
Wes Hardaker <opensource@hardakers.net>
2025-01-23 12:20:32 -0800

    refactor into two functions

--- pyfsdb/tests/test_utf8.py
+++ pyfsdb/tests/test_utf8.py
@@ -2,14 +2,18 @@ import pyfsdb
 from io import StringIO
 
 
-def test_utf8_support():
-    DATA = "#fsdb -F t test:i copy©:i foo:a\n1\t2\t3\n4\t5\t©\n"
-
-    DATA_stream = StringIO(DATA)
-    fh = pyfsdb.Fsdb(file_handle=DATA_stream)
+def do_test_utf8_file_handle(fh):
     row = next(fh)
     assert fh.column_names == ["test", "copy©", "foo"]
     assert row == [1, 2, "3"]
 
     row = next(fh)
     assert row == [4, 5, "©"]
+
+
+def test_utf8_support():
+    DATA = "#fsdb -F t test:i copy©:i foo:a\n1\t2\t3\n4\t5\t©\n"
+
+    DATA_stream = StringIO(DATA)
+    fh = pyfsdb.Fsdb(file_handle=DATA_stream)
+    do_test_utf8_file_handle(fh)
