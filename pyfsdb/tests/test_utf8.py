bc9b161c363785e1999df821a5585145a775f20f
Wes Hardaker <opensource@hardakers.net>
2025-01-23 12:19:35 -0800

    test utf 8 data

--- pyfsdb/tests/test_utf8.py
+++ pyfsdb/tests/test_utf8.py
@@ -3,10 +3,13 @@ from io import StringIO
 
 
 def test_utf8_support():
-    DATA = "#fsdb -F t test:i copy©:c foo:i\n1	2	3\n"
+    DATA = "#fsdb -F t test:i copy©:i foo:a\n1\t2\t3\n4\t5\t©\n"
 
     DATA_stream = StringIO(DATA)
     fh = pyfsdb.Fsdb(file_handle=DATA_stream)
     row = next(fh)
     assert fh.column_names == ["test", "copy©", "foo"]
-    assert row == [1, 2, 3]
+    assert row == [1, 2, "3"]
+
+    row = next(fh)
+    assert row == [4, 5, "©"]
