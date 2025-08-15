1101144135036f3fdbea941c1f1993c48b18c8c6
Wes Hardaker <opensource@hardakers.net>
2025-08-15 07:56:54 -0700

    a test for missing column data

---
+++ pyfsdb/tests/test_missing_columns.py
@@ -0,0 +1,15 @@
+import unittest
+from io import StringIO
+import pyfsdb
+
+
+class test_missing_columns(unittest.TestCase):
+    def test_fill_nones_expected(self):
+        indata = StringIO("#fsdb -F s a b c\na 1 2\na 4\n")
+
+        with pyfsdb.Fsdb(file_handle=indata) as fh:
+            row = next(fh)
+            self.assertEqual(row, ["a", "1", "2"])
+
+            row = next(fh)
+            self.assertEqual(row, ["a", "4", ""])
