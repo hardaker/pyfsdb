44c957236fe6b4535f94c5fab4cd66995c965122
Wes Hardaker <opensource@hardakers.net>
2026-01-14 14:47:55 -0800

    test filling of multiple columns

--- pyfsdb/tests/test_zero_fill.py
+++ pyfsdb/tests/test_zero_fill.py
@@ -15,7 +15,6 @@ def test_fillempty_single_column():
     with pyfsdb.Fsdb(
         file_handle=indata,
         out_file_handle=outdata,
-        out_column_names=["no", "third"],
     ) as fh:
         outdata.close = noop
         fill_values(fh, key_column="a", columns="b", value=42, bin_size=60)
@@ -35,6 +34,37 @@ def test_fillempty_single_column():
         ]
 
 
+def test_fillempty_multiple_columns():
+    """Tests that proper bin-filling happens with multiple columns"""
+    indata = StringIO("#fsdb -F s a:a b:l c:l\n60 1 11\n120 2 22\n240 4 44\n480 8 88")
+    outdata = StringIO()
+
+    def noop():
+        pass
+
+    results = None
+    with pyfsdb.Fsdb(
+        file_handle=indata,
+        out_file_handle=outdata,
+    ) as fh:
+        outdata.close = noop
+        fill_values(fh, key_column="a", columns=["b", "c"], value=42, bin_size=60)
+        results = outdata.getvalue()
+
+    with pyfsdb.Fsdb(file_handle=StringIO(results)) as fh:
+        data = fh.get_all()
+        assert data == [
+            ["60", 1, 11],
+            ["120", 2, 22],
+            ["180", 42, 42],
+            ["240", 4, 44],
+            ["300", 42, 42],
+            ["360", 42, 42],
+            ["420", 42, 42],
+            ["480", 8, 88],
+        ]
+
+
 def test_fillempty_copy_other_keys():
     """Check that multiple key filling is supported (copied from previous)"""
     indata = StringIO("#fsdb -F s a:a b:l c:a\n60 1 aa\n120 2 bb\n240 4 cc\n480 8 dd")
@@ -47,7 +77,6 @@ def test_fillempty_copy_other_keys():
     with pyfsdb.Fsdb(
         file_handle=indata,
         out_file_handle=outdata,
-        out_column_names=["no", "third"],
     ) as fh:
         outdata.close = noop
         fill_values(
