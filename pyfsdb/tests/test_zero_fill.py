c64ecef7c301c6a1f072ab9508f29c31c243ff6f
Wes Hardaker <opensource@hardakers.net>
2026-01-14 14:45:29 -0800

    test copying multiple values from other columns/keys

--- pyfsdb/tests/test_zero_fill.py
+++ pyfsdb/tests/test_zero_fill.py
@@ -33,3 +33,37 @@ def test_fillempty_single_column():
             ["420", 42],
             ["480", 8],
         ]
+
+
+def test_fillempty_copy_other_keys():
+    """Check that multiple key filling is supported (copied from previous)"""
+    indata = StringIO("#fsdb -F s a:a b:l c:a\n60 1 aa\n120 2 bb\n240 4 cc\n480 8 dd")
+    outdata = StringIO()
+
+    def noop():
+        pass
+
+    results = None
+    with pyfsdb.Fsdb(
+        file_handle=indata,
+        out_file_handle=outdata,
+        out_column_names=["no", "third"],
+    ) as fh:
+        outdata.close = noop
+        fill_values(
+            fh, key_column="a", columns="b", value=42, bin_size=60, other_keys="c"
+        )
+        results = outdata.getvalue()
+
+    with pyfsdb.Fsdb(file_handle=StringIO(results)) as fh:
+        data = fh.get_all()
+        assert data == [
+            ["60", 1, "aa"],
+            ["120", 2, "bb"],
+            ["180", 42, "bb"],
+            ["240", 4, "cc"],
+            ["300", 42, "cc"],
+            ["360", 42, "cc"],
+            ["420", 42, "cc"],
+            ["480", 8, "dd"],
+        ]
