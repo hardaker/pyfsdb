7e33c6de04c74d1bf3e78ad8a7aae58185a541e5
Wes Hardaker <opensource@hardakers.net>
2026-01-23 01:49:05 -0800

    rework placements

--- pyfsdb/graph_utils.py
+++ pyfsdb/graph_utils.py
@@ -65,23 +65,16 @@ def set_graph_parameters(control: Axes | FacetGrid, args: Namespace):
     """Set titles, labels, etc according to arguments passed."""
 
     keyword_maps = {
-        "ul": "upper left",
-        "ur": "upper right",
-        "ll": "lower left",
-        "lr": "lower right",
-        "cl": "center left",
-        "cr": "center right",
-        "c": "center",
-        "uc": "upper center",
-        "lc": "lower center",
+        "ul": {"args": ["upper left"], "kwargs": {"bbox_to_anchor": (0.1, 0.9)}},
+        "ur": {"args": ["upper right"]},
+        "ll": {"args": ["lower left"], "kwargs": {"bbox_to_anchor": (0.1, 0.3)}},
+        "lr": {"args": ["lower right"], "kwargs": {"bbox_to_anchor": (0.95, 0.3)}},
+        "cl": {"args": ["center left"], "kwargs": {"bbox_to_anchor": (0.1, 0.6)}},
+        "cr": {"args": ["center right"], "kwargs": {"bbox_to_anchor": (0.95, 0.6)}},
+        "c": {"args": ["center"]},
+        "uc": {"args": ["upper center"], "kwargs": {"bbox_to_anchor": (0.5, 0.9)}},
+        "lc": {"args": ["lower center"]},
     }
-
-    if args.legend_location:
-        sns.move_legend(
-            # maybe translate the brief name to a full one
-            control,
-            keyword_maps.get(args.legend_location, args.legend_location),
-        )
 
     if args.title:
         control.set(title=args.title)
@@ -89,6 +82,17 @@ def set_graph_parameters(control: Axes | FacetGrid, args: Namespace):
         control.set(xlabel=args.xlabel)
     if args.ylabel:
         control.set(ylabel=args.ylabel)
+
+    if args.legend_location:
+        # maybe translate the brief name to a full one
+        pass_args = args.legend_location
+        pass_kwargs = {}
+        if args.legend_location in keyword_maps:
+            pass_args = keyword_maps[args.legend_location]["args"]
+            pass_kwargs = keyword_maps[args.legend_location].get("kwargs", {})
+        else:
+            pass_args = [args]
+        sns.move_legend(control, *pass_args, **pass_kwargs)
 
 
 def output_plot(plt, output_file, args):
