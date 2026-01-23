cecb04ba61e275ea7bbc7d188d167f2da622d565
Wes Hardaker <opensource@hardakers.net>
2026-01-23 01:19:31 -0800

    convert command parsing to pytest

--- pyfsdb/tests/test_command_parsing.py
+++ pyfsdb/tests/test_command_parsing.py
@@ -1,63 +1,76 @@
+import pytest
 import pyfsdb
 import unittest
 from io import StringIO
 from logging import error
 
 
-class TestCommandParsing(unittest.TestCase):
-    commands = ["command1", "command2"]
-    DATA_FILE = "pyfsdb/tests/tests.fsdb"
-    COMP_FILE = "pyfsdb/tests/testscomp.fsdb.xz"
-    test_data = "#fsdb -f s a b c\n1 2 3\n4 5 6\n# | command one"
-    ROW1 = ["rowone", "info", "data"]
-    ROW2 = ["rowtwo", "other", "stuff"]
+@pytest.fixture
+def DATA_FILE(tmp_path):
+    content = """#fsdb -F t colone coltwo colthree
+rowone	info	data
+# middle comment
+rowtwo	other	stuff
+#  | command1
+#   | command2
+"""
+    tmpfile = tmp_path / "test.fsdb"
+    tmpfile.write_text(content, encoding="utf-8")
+    return tmpfile
 
-    def test_history_from_stringio_fails(self):
-        test_file = StringIO(self.test_data)
-        fh = pyfsdb.Fsdb(file_handle=test_file)
 
-        history_data = fh.commands
-        self.assertEqual(history_data, None)
+commands = ["command1", "command2"]
+COMP_FILE = "pyfsdb/tests/testscomp.fsdb.xz"
+test_data = "#fsdb -f s a b c\n1 2 3\n4 5 6\n# | command one"
+ROW1 = ["rowone", "info", "data"]
+ROW2 = ["rowtwo", "other", "stuff"]
 
-    def test_get_commands_at_end(self):
-        fh = pyfsdb.Fsdb(self.DATA_FILE)
-        fh.get_all()
-        read_commands = fh.commands
-        self.assertEqual(self.commands, read_commands)
+def test_history_from_stringio_fails():
+    test_file = StringIO(test_data)
+    fh = pyfsdb.Fsdb(file_handle=test_file)
 
-    def test_get_commands_before_end(self):
-        fh = pyfsdb.Fsdb(self.DATA_FILE)
-        read_commands = fh.commands
-        self.assertEqual(next(fh), self.ROW1)
-        self.assertEqual(self.commands, read_commands)
+    history_data = fh.commands
+    assert history_data is None
 
-        # make sure we can read data too even after reading ahead
-        self.assertEqual(next(fh), self.ROW2)
+def test_get_commands_at_end(DATA_FILE):
+    fh = pyfsdb.Fsdb(DATA_FILE)
+    fh.get_all()
+    read_commands = fh.commands
+    assert commands == read_commands
 
-    def test_compressed_files(self):
-        # ensure we can test thsi
-        try:
-            import lzma
-        except Exception:
-            return
+def test_get_commands_before_end(DATA_FILE):
+    fh = pyfsdb.Fsdb(DATA_FILE)
+    read_commands = fh.commands
+    assert next(fh) == ROW1
+    assert commands == read_commands
 
-        fh = pyfsdb.Fsdb(self.COMP_FILE)
-        row = next(fh)
-        self.assertEqual(row, self.ROW1)
+    # make sure we can read data too even after reading ahead
+    assert next(fh) == ROW2
 
-    def test_command_gathering_in_compressed(self):
-        # ensure we can test thsi
-        try:
-            import lzma
-        except Exception:
-            return
+def test_compressed_files():
+    # ensure we can test thsi
+    try:
+        import lzma
+    except Exception:
+        return
 
-        fh = pyfsdb.Fsdb(self.COMP_FILE)
-        row = next(fh)
-        self.assertEqual(row, self.ROW1)
+    fh = pyfsdb.Fsdb(COMP_FILE)
+    row = next(fh)
+    assert row == ROW1
 
-        test_commands = fh.commands
-        self.assertEqual(test_commands, None)  # None == failure to read
+def test_command_gathering_in_compressed():
+    # ensure we can test thsi
+    try:
+        import lzma
+    except Exception:
+        return
 
-        row = next(fh)
-        self.assertEqual(row, self.ROW2)
+    fh = pyfsdb.Fsdb(COMP_FILE)
+    row = next(fh)
+    assert row == ROW1
+
+    test_commands = fh.commands
+    assert test_commands is None  # None == failure to read
+
+    row = next(fh)
+    assert row == ROW2
